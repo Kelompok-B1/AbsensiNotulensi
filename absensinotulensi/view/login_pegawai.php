54bfdc23685cbd181864a1cc2ebd5943ba776c35
andikayudh182 <61397720+andikayudh182@users.noreply.github.com>
2021-07-15 10:35:55 +0700

    update div login

--- absensinotulensi/view/login_pegawai.php
+++ absensinotulensi/view/login_pegawai.php
@@ -17,35 +17,39 @@
       <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
         <div class="card card-signin my-5">
           <div class="card-body">
-            <h4 class="card-title text-center">Sign In</h4><p>
-            <form class="form-signin">
+            <h4 class="card-title text-center">Login Pegawai</h4><p>
+         
               <div class="form-label-group">
-         <form method="POST" action="proses_login_pegawai.php">
-            <div class="form-group">
-                <input type="text" class="form-control" name="username" aria-describedby="emailHelp" placeholder="Username" required autofocus>
-               </div>
-            <div class="form-group">
-                <input type="password" class="form-control" name="password" placeholder="Password" required>
+         
+                <form method="POST" action="proses_login_pegawai.php">
+                        <div class="form-group">
+                            <input type="text" class="form-control" name="username" aria-describedby="emailHelp" placeholder="Username" required autofocus>
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-control" name="password" placeholder="Password" required>
+                        </div>
+                        <a href= "../MainFrame/index.html" button type="submit" name="login" class="btn btn-warning" style="width:49%;">Back</button></a>
+                        <button type="submit" name="login" class="btn btn-primary" style="width:49%;">Login</button>
+                        <?php 
+                            if(isset($_GET['pesan'])){
+                                if($_GET['pesan'] == "gagal"){
+                                    echo"
+                                    <script>alert('Login gagal! Username dan Password Salah!')</script>;
+                                    ";
+                                }else if($_GET['pesan'] == "logout"){
+                                    echo " <script>alert('Anda Telah Berhasil Logout')</script>;";
+                                }else if($_GET['pesan'] == "belum_login"){
+                                echo " <script>alert('Silahkan Login Terlebih Dahulu Untuk Masuk Ke Halaman Pegawai')</script>";
+                            }
+                        }
+                    ?>
+                </form>
             </div>
-                <a href= "../MainFrame/index.html" button type="submit" name="login" class="btn btn-warning" style="width:49%;">Back</button></a>
-                <button type="submit" name="login" class="btn btn-primary" style="width:49%;">Login</button>
-                <?php 
-                    if(isset($_GET['pesan'])){
-                        if($_GET['pesan'] == "gagal"){
-                            echo"
-                            <script>alert('Login gagal! Username dan Password Salah!')</script>;
-                            ";
-                        }else if($_GET['pesan'] == "logout"){
-                            echo " <script>alert('Anda Telah Berhasil Logout')</script>;";
-                        }else if($_GET['pesan'] == "belum_login"){
-                        echo " <script>alert('Silahkan Login Terlebih Dahulu Untuk Masuk Ke Halaman Pegawai')</script>";
-                    }
-                }
-	?>
-        </form>
-        
+          </div>       
+        </div>
+      </div>
     </div>
-
+  </div>
 </body>
 
 
